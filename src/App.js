3101dbe898cf3a59cf17b09516e582ce0b558d68
FirePizza <dev.firepizza@gmail.com>
2022-01-12 12:15:07 +0900

    pull request를 머지할때 액션 테스트

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello react
+          Pull Request!
         </p>
         <a
           className="App-link"
